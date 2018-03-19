4df1e1f7c295f33e8d34667f59470a0696230bf5
Ram Sharma <ramsharma.india001@gmail.com>
2018-03-19 23:26:08 +0530

    Session on 19/03

---
+++ JavaConcepts/src/fundamentals/DataTypeDemo.java
@@ -0,0 +1,28 @@
+package fundamentals;
+
+public class DataTypeDemo {
+
+	char cch;
+	static char c;
+	
+	public static void main(String[] args) {
+		//int x = 20;
+		byte b = 43;
+		int y = b;
+		
+		short s = (short)y;
+		
+		//int i = 8323023892383;
+		long l = 832302386786987l;
+		
+		float f = 2.3f;
+		
+		c = 56;
+		char ch = 'a';
+		char chh;
+		chh = 'a';
+		
+		
+	}
+
+}
