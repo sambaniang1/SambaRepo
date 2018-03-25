0671e37ad83d831db64b823470ee6630d1dda6c7
Ram Sharma <ramsharma.india001@gmail.com>
2018-03-25 13:15:19 +0530

    Session on 03/25

--- JavaConcepts/src/fundamentals/ArrayDemo.java
+++ JavaConcepts/src/fundamentals/ArrayDemo.java
@@ -9,9 +9,60 @@ public class ArrayDemo {
 		iArr[2] = 56;
 		iArr[3] = 23;
 		
+		/*for(int num: iArr){
+			System.out.println(num);
+		}*/
+		
+		/*for(int i=0;i<iArr.length;i++){
+			System.out.println(iArr[i]);
+		}*/
+		
 		int[] jArr = {4, -90, 56, 23};
 		
 		int[][] twoDArr = new int[3][4];
+		
+	/*	System.out.println(twoDArr.length);
+		System.out.println(twoDArr[0].length);*/
+		
+		twoDArr[0][0] = 1;
+		twoDArr[0][1] = 2;
+		twoDArr[0][2] = 3;
+		twoDArr[0][3] = 4;
+		
+		twoDArr[1][0] = 5;
+		twoDArr[1][1] = 6;
+		twoDArr[1][2] = 7;
+		twoDArr[1][3] = 8;
+		
+		twoDArr[2][0] = 9;
+		twoDArr[2][1] = 10;
+		twoDArr[2][2] = 11;
+		twoDArr[2][3] = 12;
+		
+		int[][] twoDArr2 = {{1, 2, 3, 4, 80}, {5, 6, 7}, {9, 10, 11, 12}};
+		
+		/*for(int[] oneDArr: twoDArr2){
+			for(int num: oneDArr){
+				System.out.print(num+" ");
+			}
+			System.out.println();
+		}*/
+		
+		for(int i =0;i<twoDArr2.length;i++){
+			int[] oneDArr = twoDArr2[i];
+			for(int j =0;j<oneDArr.length;j++){
+				System.out.println(twoDArr2[i][j]);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
---
+++ JavaConcepts/src/operators/OperatorDemo.java
@@ -0,0 +1,40 @@
+package operators;
+
+public class OperatorDemo {
+
+	public static void main(String[] args) {
+		
+		/*int i = 6;
+		int j = i++; // In post increment, first it gives value and then increments
+		int k = ++i; // in pre increment , first it increments and then give value
+		int l = --i;
+		int m = i--;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+		System.out.println(m);*/
+		
+		/*int a = 10;
+		int b = ++a + a++ - --a - a--; // 11 + 11 - 11 - 11
+		System.out.println(a);
+		System.out.println(b);*/
+		
+		/*int i = 10;
+		String s = "10";
+		
+		System.out.println(i+1);
+		System.out.println(s+1);*/
+		
+		/*int a = 10;
+		int b = 20;
+		a = b;
+		System.out.println(a);
+		System.out.println(b);*/
+		
+		int a=21, b=20;
+		int x = (a>b)?40:50;
+		System.out.println(x);
+	}
+
+}
